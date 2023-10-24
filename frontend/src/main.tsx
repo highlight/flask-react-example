dfbfcadc81541780497cc939716d2ee638a8c7a5
Vadim Korolik <vkorolik@gmail.com>
2023-10-24 10:11:53 -0700

    update frontend tracing origins

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,22 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ErrorBoundary } from "@highlight-run/react";
-import { H } from "highlight.run";
+import {ErrorBoundary} from "@highlight-run/react";
+import {H} from "highlight.run";
 
 H.init("4d7k1xeo", {
-  serviceName: "frontend-app",
-  tracingOrigins: true,
-  networkRecording: {
-    enabled: true,
-    recordHeadersAndBody: true,
-  },
+    serviceName: "frontend-app",
+    tracingOrigins: [
+        'python-flask-backend.onrender.com',
+        'localhost',
+        'localhost:5001',
+    ],
+    networkRecording: {
+        enabled: true,
+        recordHeadersAndBody: true,
+    },
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ErrorBoundary>
+            <App/>
+        </ErrorBoundary>
+    </React.StrictMode>,
 );
